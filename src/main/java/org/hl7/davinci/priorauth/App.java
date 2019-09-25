bbda4141f7e7d210c45ab890eff07f4779923e82
Benjamin Langley <blangley@mitre.org>
2019-09-25 12:59:39 -0400

    Remove unused import from App

--- src/main/java/org/hl7/davinci/priorauth/App.java
+++ src/main/java/org/hl7/davinci/priorauth/App.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import ca.uhn.fhir.context.FhirContext;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Timer;
