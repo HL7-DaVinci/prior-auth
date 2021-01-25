9bd8cbd5d75e3ce46d3170d06345c56568d4ff0e
Benjamin Langley <blangley@mitre.org>
2021-01-25 14:47:11 -0500

    Add authorization endpoints

--- src/main/java/org/hl7/davinci/priorauth/App.java
+++ src/main/java/org/hl7/davinci/priorauth/App.java
@@ -5,6 +5,7 @@ import org.hl7.davinci.authorization.AuthUtils;
 import org.hl7.davinci.rules.PriorAuthRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import ca.uhn.fhir.context.FhirContext;
 
@@ -13,6 +14,7 @@ import ca.uhn.fhir.context.FhirContext;
  * launched with this App.
  */
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.hl7.davinci.authorization"})
 public class App {
 
   /**
