09b2c56c33612fff508b542c15b8d1dafb7e4d63
Sahil Malhotra <smalhotra@mitre.org>
2021-12-03 18:05:30 -0500

    temp fix for failing build

--- src/main/java/org/hl7/davinci/priorauth/Database.java
+++ src/main/java/org/hl7/davinci/priorauth/Database.java
@@ -79,7 +79,9 @@ public class Database {
   }
 
   public Database() {
-    this("./databaseData/");
+    // this("./databaseData/");
+    this("./");
+
   }
 
   public Database(String relativePath) {
