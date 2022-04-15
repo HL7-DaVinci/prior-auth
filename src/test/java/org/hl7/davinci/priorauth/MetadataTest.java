209f30d4952f454369c3f9015ed7302d2e274cfc
jtquach1 <jtquach1@gmail.com>
2022-04-15 09:44:59 -0400

    Try to fix CapabilityStatement errors and refactor code - Type mismatch for CapabilityStatement.rest.extension:capabilitystatement-websocket - Type mismatch for CapabilityStatement.rest.security.extension:oauth-uris.authorize
    Update integration tests to reflect FHIR STU 4.0.1 CapabilityStatement definition

--- src/test/java/org/hl7/davinci/priorauth/MetadataTest.java
+++ src/test/java/org/hl7/davinci/priorauth/MetadataTest.java
@@ -15,10 +15,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
 import org.hl7.fhir.r4.model.CapabilityStatement;
 import org.hl7.fhir.r4.model.Extension;
 import org.hl7.fhir.r4.model.StringType;
+import org.hl7.fhir.r4.model.CapabilityStatement.CapabilityStatementRestComponent;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -64,9 +64,11 @@ public class MetadataTest {
 
     // Test the websocket extension is included
     StringType websocketUrl = null;
-    for (Extension ext : capabilityStatement.getExtension()) {
-      if (ext.getUrl().equals(FhirUtils.WEBSOCKET_EXTENSION_URL))
-        websocketUrl = (StringType) ext.getValue();
+    for (CapabilityStatementRestComponent rest : capabilityStatement.getRest()) {
+      for (Extension ext : rest.getExtension()) {
+        if (ext.getUrl().equals(FhirUtils.WEBSOCKET_EXTENSION_URL))
+          websocketUrl = (StringType) ext.getValue();
+      }
     }
     Assert.assertNotNull(websocketUrl);
 
@@ -96,9 +98,11 @@ public class MetadataTest {
 
     // Test the websocket extension is included
     StringType websocketUrl = null;
-    for (Extension ext : capabilityStatement.getExtension()) {
-      if (ext.getUrl().equals(FhirUtils.WEBSOCKET_EXTENSION_URL))
-        websocketUrl = (StringType) ext.getValue();
+    for (CapabilityStatementRestComponent rest : capabilityStatement.getRest()) {
+      for (Extension ext : rest.getExtension()) {
+        if (ext.getUrl().equals(FhirUtils.WEBSOCKET_EXTENSION_URL))
+          websocketUrl = (StringType) ext.getValue();
+      }
     }
     Assert.assertNotNull(websocketUrl);
 
